b4159d12698be984b2641efa8a8e3c5be387e206
TJ Vinsavich <tjvinsavich@gmail.com>
2021-07-01 14:04:59 -0500

    initial commit, finished exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string input)
+        {
+            var endIndex = input.Length - 1;
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != (input[endIndex - i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("duck", false)]
+        [InlineData("sassafras", false)]
+        [InlineData("madam", true)]
+        public void Test1(string input, bool expected)
         {
-
+            //Arrange
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsAPalindrome(input);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
